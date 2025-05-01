fcefcf76a759bf9a11d24a46aa37ea76fa7f268b
Sanjay <60233113+Sanjay5004@users.noreply.github.com>
2025-05-01 23:14:52 +0530

    Delete sigma-webnovel-theme directory

--- sigma-webnovel-theme/js/custom-menu.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var menu = document.getElementById("primary-menu");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
-}
